565e4770e532fe25c918aa47217b1f6d83aa06ad
kenshin <kenshin.acs@gmail.com>
2016-07-14 11:45:33 +0800

    utils and version defines

---
+++ src/util.h
@@ -0,0 +1,8 @@
+#ifndef _RPG_UTIL_H
+#define _RPG_UTIL_H
+
+
+#define CRLF    "\x0d\x0a"
+
+#endif
+
---
+++ src/version.h
@@ -0,0 +1,6 @@
+#ifndef _RPG_VERSION_H
+#define _RPG_VERSION_H
+
+#define RPG_VERSION "0.1.2"
+
+#endif
