bfe6ee8e854065a03c8b1ec58f2949fff09745a4
kenshin <kenshin.acs@gmail.com>
2016-07-14 17:58:27 +0800

    rpg_alloc(), rpg_free etc.. functions

--- src/util.c
+++ src/util.c
@@ -1,1 +1,67 @@
 #include "util.h"
+#include "log.h"
+
+#include <stdlib.h>
+
+
+void *
+_rpg_alloc(size_t size, const char *name, int line) {
+    void *p;
+    
+    ASSERT(size != 0);
+
+    p = malloc(size);
+    
+    if (p == NULL) {
+        log_error("malloc(%zu) failed @ %s:%d", size, name, line);
+    } else {
+        log_verb("malloc(%zu) at %p @ %s:%d", size, p, name, line);
+    }
+
+    return p;
+}
+
+void *
+_rpg_zalloc(size_t size, const char *name, int line) {
+    void *p;
+
+    p = _rpg_alloc(size, name, line);\
+    if (p != NULL) {
+        memset(p, 0, size);
+    }
+
+    return p;
+}
+
+void *
+_rpg_calloc(size_t nmemb, size_t size, const char *name, int line) {
+    return _rpg_alloc(nmemb * size, name, line);
+}
+
+void *
+_rpg_realloc(void *ptr, size_t size, const char *name, int line) {
+    void *p;
+    
+    ASSERT(size != 0);
+
+    p = realloc(ptr, size);
+    
+    if (p == NULL) {
+        log_error("realloc(%zu) failed @ %s:%d", size, name, line);
+    } else {
+        log_verb("realloc(%zu) at %p @ %s:%d", size, p, name, line);
+    }
+}
+
+void 
+_rpg_free(void *ptr, const char *name, int line) {
+    ASSERT(ptr != NULL);
+    log_verb("free(%p) @ %s:%d", ptr, name, line);
+    free(ptr);
+}
+
+void
+rpg_assert(const char *cond, const char *file, int line) {
+    log_error("assert '%s' failed @ (%s, %d)", cond, file, line);
+    abort();
+}
--- src/util.h
+++ src/util.h
@@ -1,8 +1,39 @@
 #ifndef _RPG_UTIL_H
 #define _RPG_UTIL_H
 
+#include <stdlib.h>
 
 #define CRLF    "\x0d\x0a"
+
+#define rpg_alloc(_s)                                               \
+    _rpg_alloc((size_t)(_s), __FILE__, __LINE__)                    \
+
+#define rpg_zalloc(_s)                                              \
+    _rpg_zalloc((size_t)(_s), __FILE__, __LINE__)                   \
+
+#define rpg_calloc(_n, _s)                                          \
+    _rpg_calloc((size_t)(_n), (size_t)(_s), __FILE__, __LINE__)     \
+
+#define rpg_realloc(_p, _s)                                         \
+    _rpg_realloc(_p, (size_t)(_s), __FILE__, __LINE__)              \
+
+#define rpg_free(_p)                                                \
+    _rpg_free(_p, __FILE__, __LINE__)                               \
+
+void  *_rpg_alloc(size_t size, const char *name, int line);
+void * _rpg_zalloc(size_t size, const char *name, int line);
+void *_rpg_calloc(size_t nmemb, size_t size, const char *name, int line);
+void *_rpg_realloc(void *ptr, size_t size, const char *name, int line);
+void _rpg_free(void *ptr, const char *name, int line);
+
+
+#define ASSERT(_x) do {                                             \
+    if (!(_x)) {                                                    \
+        rpg_assert(#_x, __FILE__, __LINE__);                        \
+    }                                                               \
+} while(0)
+
+void rpg_assert(const char *cond, const char *file, int line);
 
 #endif
 
