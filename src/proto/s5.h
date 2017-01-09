b801258fdebe60fe04d4702826d5b9641046feb1
kenshin <kenshin.acs@gmail.com>
2017-01-09 17:49:53 +0800

    Ascii Art: RPS work based socks5 proxy

--- src/proto/s5.h
+++ src/proto/s5.h
@@ -5,6 +5,56 @@
 
 #include <uv.h>
 
+/*
+ * RPS work based on socks5 proxy 
+ * https://www.ietf.org/rfc/rfc1928.txt
+ *
+ *                  Client                RPS               Upstream            Remote
+ *
+ *                   |                    |                    |                 |
+ *                   |     S5_Version     |                    |                 |
+ *                   |  +------------->   |                    |                 |
+ *                   |                    |                    |                 |
+ *     S5 HandShake  |     S5_Method      |                    |                 |
+ *  +--------------+ |  <--------------+  |                    |                 |
+ *                   |                    |                    |                 |
+ *                   |                    |                    |                 |
+ *                   |    S5_Auth         |                    |                 |
+ *                   |   +------------->  |                    |                 |
+ *                   |                    |                    |                 |
+ *   S5 SubNegotiate |    S5_Auth_result  |                    |                 |
+ *  +--------------+ |   <--------------+ |                    |                 |
+ *                   |                    |                    |                 |
+ *                   |                    |                    |                 |
+ *                   |    S5_Request      |                    |                 |
+ *                   |   +------------->  |    Connect         |                 |
+ *                   |                    |  +------------->   |                 |
+ *                   |                    |                    |                 |
+ *                   |                    |     S5_Version     |                 |
+ *                   |                    |  +------------->   |                 |
+ *                   |                    |                    |                 |
+ *                   |      S5 HandShake  |     S5_Method      |                 |
+ *                   |    +-------------+ |  <--------------+  |                 |
+ *                   |                    |                    |                 |
+ *                   |                    |                    |                 |
+ *                   |                    |    S5_Auth         |                 |
+ *                   |                    |   +------------->  |                 |
+ *                   |                    |                    |                 |
+ *                   |    S5 SubNegotiate |    S5_Auth_result  |                 |
+ *                   |   +--------------+ |   <--------------- |                 |
+ *                   |                    |                    |                 |
+ *                   |                    |                    |                 |
+ *                   |                    |    S5_Request      |                 |
+ *                   |                    |   +------------->  |   Connect       |
+ *                   |                    |                    | +-------------> |
+ *                   |                    |    S5_Reply        |                 |
+ *                   |                    |   <-------------+  |                 |
+ *                   |                    |                    |                 |
+ *                   |    S5_Reply        |                    |                 |
+ *                   |   <-------------+  |                    |                 |
+ *RPS Handshake sucess                    |                    |                 |
+ *+------------------|                    |                    |                 |
+ *                   |                    |                    |                 | */
 
 void s5_do_parse(struct context *ctx, const char *data, ssize_t nread);
 
