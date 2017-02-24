6fe5da9245046ec06dccb1f14e3b5bb60bb04946
kenshin <kenshin.acs@gmail.com>
2017-02-24 12:30:45 +0800

    define proxy and proxy_pool struct

---
+++ src/proxy.c
@@ -0,0 +1,9 @@
+#include "proxy.h"
+
+#include "util.h"
+
+void
+proxy_init(struct proxy *p) {
+    UNUSED(p);
+}
+
---
+++ src/proxy.h
@@ -0,0 +1,23 @@
+#ifndef _PROXY_H
+#define _PROXY_H
+
+#include "core.h"
+#include "util.h"
+#include "array.h"
+#include "_string.h"
+
+struct proxy  {
+    rps_addr_t  server;
+    rps_str_t   uname;
+    rps_str_t   passwd;
+    rps_proxy_t proto;
+};
+
+struct proxy_pool {
+    rps_array_t pool;
+    uint32_t    index;
+};
+
+void proxy_init(struct proxy *p);
+
+#endif
