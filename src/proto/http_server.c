28be5fcfc09de7bcbebe3dc4bf73ab4c2bf0c788
kenshin <kenshin.acs@gmail.com>
2017-03-21 18:42:25 +0800

    mold of http do handshake and do auth

--- src/proto/http_server.c
+++ src/proto/http_server.c
@@ -6,24 +6,36 @@
 
 
 static void
-http_do_handshake(struct context *ctx) {
+http_do_handshake(struct context *ctx, uint8_t *data, size_t size) {
     
+    printf("begin do http handshake, read %zd bytes: %s\n", size, data);
 }
 
 static void
-http_do_auth(struct context *ctx) {
+http_do_auth(struct context *ctx, uint8_t *data, size_t size) {
+    printf("begin do http auth, read %zd bytes: %s\n", size, data);
 
 }
 
 
 void
 http_server_do_next(struct context *ctx) {
-    char    *data;
+    uint8_t *data;
     ssize_t size;
 
-    data = ctx->rbuf;
-    size = ctx->nread;
+    data = (uint8_t *)ctx->rbuf;
+    size = (size_t)ctx->nread;
 
-    printf("http read %zd bytes: %s\n", size, data);
+
+    switch (ctx->state) {
+        case c_handshake:
+            http_do_handshake(ctx, data, size);
+            break;
+        case c_auth:
+            http_do_auth(ctx, data, size);
+            break;
+        default:
+            NOT_REACHED();
+    }
 }
 
