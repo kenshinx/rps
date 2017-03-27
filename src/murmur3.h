42f04bb2edb6c10b8f16f0ee9fdfe0a95fb5a0ca
kenshin <kenshin.acs@gmail.com>
2017-03-27 17:39:03 +0800

    Remove the Executable mode

--- src/murmur3.h
+++ src/murmur3.h
@@ -8,10 +8,6 @@
 
 #include <stdint.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 //-----------------------------------------------------------------------------
 
 void MurmurHash3_x86_32 (const void *key, int len, uint32_t seed, void *out);
@@ -21,9 +17,5 @@ void MurmurHash3_x86_128(const void *key, int len, uint32_t seed, void *out);
 void MurmurHash3_x64_128(const void *key, int len, uint32_t seed, void *out);
 
 //-----------------------------------------------------------------------------
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif // _MURMURHASH3_H_
