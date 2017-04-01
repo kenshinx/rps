46ce95590acf8b9abdd544f0f4767f701d999352
kenshin <kenshin.acs@gmail.com>
2017-04-01 18:16:17 +0800

    Begin http client side implementation

--- src/proto/http_client.c
+++ src/proto/http_client.c
@@ -4,7 +4,27 @@
 
 #include <uv.h>
 
+static void
+http_do_handshake(struct context *ctx) {
+    struct http_request req;
+
+    http_request_init(&req);
+    
+    req.method = http_get;
+    
+
+}
+
 
 void
 http_client_do_next(struct context *ctx) {
+    
+    switch (ctx->state) {
+        case c_handshake_req:
+            http_do_handshake(ctx);
+            break;
+
+        default:
+            NOT_REACHED();
+    }
 }
