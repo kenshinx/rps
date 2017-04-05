2f0ca5f8700de994d0f7bcfe101f5d0552d6eb1c
kenshin <kenshin.acs@gmail.com>
2017-04-05 18:11:51 +0800

    http client do handshake and send request

--- src/proto/http_client.c
+++ src/proto/http_client.c
@@ -4,15 +4,67 @@
 
 #include <uv.h>
 
-static void
-http_do_handshake(struct context *ctx) {
+static int
+http_send_request(struct context *ctx) {
     struct http_request req;
+    rps_addr_t  *remote;
+    struct upstream *u;
+    char message[HTTP_MESSAGE_MAX_LENGTH];
+    int len;
+
+    remote = &ctx->sess->remote;
+    ASSERT(remote->family == AF_DOMAIN);
 
     http_request_init(&req);
     
-    req.method = http_get;
-    
+    req.method = http_connect;
+    req.port = (int)remote->addr.name.port;
+    string_duplicate(&req.host, remote->addr.name.host, strlen(remote->addr.name.host));
+    string_duplicate(&req.protocol, HTTP_DEFAULT_PROTOCOL, strlen(HTTP_DEFAULT_PROTOCOL));
 
+    const char key1[] = "Host";
+    char val1[HTTP_HEADER_MAX_VALUE_LENGTH];
+    int v1len;
+    
+    v1len = snprintf(val1, HTTP_HEADER_MAX_VALUE_LENGTH, "%s:%d", req.host.data, req.port);
+    hashmap_set(&req.headers, (void *)key1, strlen(key1), (void *)val1, v1len);
+
+#ifdef HTTP_PROXY_AGENT
+    const char key2[] = "Proxy-Agent";
+    hashmap_set(&req.headers, (void *)key2, strlen(key2), 
+            (void *)HTTP_DEFAULT_PROXY_AGENT, strlen(HTTP_DEFAULT_PROXY_AGENT));
+#endif
+
+    u = &ctx->sess->upstream;
+    
+    if (!string_empty(&u->uname)) {
+        /* autentication required */
+        const char key3[] = "Proxy-Authenticate";
+        char val3[HTTP_HEADER_MAX_VALUE_LENGTH];   
+        int v3len;
+        
+        v3len = http_basic_auth_gen((const char *)u->uname.data, 
+                (const char *)u->passwd.data, val3);
+        hashmap_set(&req.headers, (void *)key3, strlen(key3), (void *)val3, v3len);
+    }
+
+    len = http_request_message(message, &req);
+
+    ASSERT(len > 0);
+
+    http_request_deinit(&req);
+
+    return server_write(ctx, message, len);
+}
+
+static void
+http_do_handshake(struct context *ctx) {
+    if (http_send_request(ctx) != RPS_OK) {
+        ctx->state = c_retry;
+        server_do_next(ctx);
+    } else {
+        ctx->state = c_handshake_resp;
+    }
 }
 
 
