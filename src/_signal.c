1eb2c5ce3b6336fc056cffe485007b34ff71ccc5
kenshin <kenshin.acs@gmail.com>
2017-05-24 19:14:17 +0800

    signal handler

--- src/_signal.c
+++ src/_signal.c
@@ -9,8 +9,8 @@ static struct _signal signals[] = {
     { SIGUSR2, "SIGUSR2", 0,                 signal_handler },
     { SIGTTIN, "SIGTTIN", 0,                 signal_handler },
     { SIGTTOU, "SIGTTOU", 0,                 signal_handler },
-    { SIGHUP,  "SIGHUP",  0,                 signal_handler },
     { SIGINT,  "SIGINT",  0,                 signal_handler },
+    { SIGINT,  "SIGTERM", 0,                 signal_handler },
     { SIGSEGV, "SIGSEGV", (int)SA_RESETHAND, signal_handler },
     { SIGPIPE, "SIGPIPE", 0,                 SIG_IGN },
     { 0,        NULL,     0,                 NULL }
@@ -43,5 +43,66 @@ signal_init(void) {
 
 void
 signal_handler(int signo) {
+	struct _signal *sig;
+    void (*action)(void);
+    char *actionstr;
+    bool done;
 
+    for (sig = signals; sig->signo != 0; sig++) {
+        if (sig->signo == signo) {
+            break;
+        }
+    }
+    ASSERT(sig->signo != 0);
+
+    actionstr = "";
+    action = NULL;
+    done = false;
+
+    switch (signo) {
+    case SIGUSR1:
+        break;
+
+    case SIGUSR2:
+        break;
+
+    case SIGTTIN:
+        actionstr = ", up logging level";
+        action = log_level_up;
+        break;
+
+    case SIGTTOU:
+        actionstr = ", down logging level";
+        action = log_level_down;
+        break;
+
+    case SIGINT:
+        done = true;
+        actionstr = ", exiting";
+        break;
+
+    case SIGTERM:
+        done = true;
+        actionstr = ", terminating";
+        break;
+
+    case SIGSEGV:
+        //log_stacktrace();
+        actionstr = ", core dumping";
+        raise(SIGSEGV);
+        break;
+
+    default:
+        NOT_REACHED();
+    }
+
+    log_notice("signal %d (%s) received%s", signo, sig->signame, actionstr);
+
+    if (action != NULL) {
+        action();
+    }
+
+    if (done) {
+        exit(1);
+    }
 }
