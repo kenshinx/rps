72c966d4516cb6aeb2327756067c8764e4aa195f
kenshin <kenshin.acs@gmail.com>
2017-07-20 11:56:27 +0800

    http proxy protocol

---
+++ src/proto/http_proxy.h
@@ -0,0 +1,71 @@
+#ifndef _RPS_HTTP_PROXY_H
+#define _RPS_HTTP_PROXY_H
+
+#include "http.h"
+
+
+/*
+ * Related RFC:
+ *
+ * Hypertext Transfer Protocol -- HTTP/1.1
+ * https://tools.ietf.org/html/rfc2616
+ *
+ * HTTP Authentication: Basic and Digest Access Authentication
+ * https://tools.ietf.org/html/rfc2617
+ * 
+ *
+ * RPS http proxy establishment procedure
+ * 
+ *             Client                   RPS                 Upstream        Remote
+ *
+ *              +                        +                       +              +
+ *   Request    |    HTTP Request        |                       |              |
+ *  +--------+  | +------------------>   |                       |              |
+ *              |  Host:                 |                       |              |
+ *              |  Proxy_Authorization:  |                       |              |
+ *              |  (Maybe)               |                       |              |
+ *              |                        |                       |              |
+ *              |                        |                       |              |
+ *              |                        |                       |              |
+ *  Response    |  HTTP 407 Auth Require |                       |              |
+ * +------------+ <-------------------+  |                       |              |
+ *              |                        |                       |              |
+ *              |                        |                       |              |
+ *  Re-Request  |  HTTP Request          |                       |              |
+ * +------------+ +--------------------> |                       |              |
+ *              |  Host:                 |                       |              |
+ *              |  Proxy_Authorization:  |                       |              |
+ *              |                        |                       |              |
+ *              |                        |                       |              |
+ *              |                        |                       |              |
+ *              |                        |                       |              |
+ *              |                        |  HTTP Request         |              |
+ *              |                        |  +---------------->   |              |
+ *              |                        |  Host:                |              |
+ *              |                        |  Proxy_Authorization: |              |
+ *              |                        |  (Maybe)              |              |
+ *              |                        |                       |              |
+ *              |                        |                       |              |
+ *              |                        |                       |              |
+ *              |                        | HTTP 407 Auth Require |              |
+ *              |                        | <-------------------+ |              |
+ *              |                        |                       |              |
+ *              |                        |                       |              |
+ *              |                        |  HTTP Request         |              |
+ *              |                        |  +------------------> |              |
+ *              |                        |  Host:                | HTTP Request |
+ *              |                        |  Proxy_Authorization: | +----------> |
+ *              |                        |                       |              |
+ *              |                        |                       |              |
+ *              |                        |                       |              |
+ *              |                        |                       |              |
+ *              |                        |                       | HTTP Response|
+ *              |                        |     HTTP Response     | <----------+ |
+ *  Finished    |    HTTP Response       |  <-----------------+  |              |
+ * +----------+ | <-------------------+  |                       |              |
+ *              |                        |                       |              |
+ *              +                        +                       +              +
+*/
+
+
+#endif
