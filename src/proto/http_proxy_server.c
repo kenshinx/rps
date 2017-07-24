c9a6b685d9642fbb1160602e42a92a2efb54af50
kenshin <kenshin.acs@gmail.com>
2017-07-24 16:50:07 +0800

    Server endpoint HTTP proxy protocol

--- src/proto/http_proxy_server.c
+++ src/proto/http_proxy_server.c
@@ -4,24 +4,41 @@
 static void
 http_proxy_parse_request(struct context *ctx) {
     int http_verify_result;
+    struct http_request *req;
 
     http_verify_result = http_request_verify(ctx);
 
+    if (ctx->req == NULL) {
+        log_verb("http proxy client request error");
+        ctx->state = c_kill;
+        server_do_next(ctx);
+        return;
+    }
+
+    req = (struct http_request *)ctx->req;
+
     switch (http_verify_result) {
     case http_verify_error:
-        log_verb("http client request error");
         ctx->state = c_kill;
-        break;
-    case http_verify_success:
-        ctx->state = c_exchange;
-        log_verb("http client request success");
+        log_verb("http proxy client %s %s error", 
+                http_method_str(req->method), req->full_uri.data);
         break;
     case http_verify_fail:
         ctx->state = c_auth_resp;
-        log_verb("http client request authentication required");
+        log_verb("http proxy client %s %s need authentication", 
+                http_method_str(req->method), req->full_uri.data);
         break;
+    case http_verify_success:
+        ctx->state = c_exchange;
+        log_verb("http proxy client %s %s success", 
+                http_method_str(req->method), req->full_uri.data);
+        server_do_next(ctx);
+        return;
     }
 
+    http_request_deinit(ctx->req);
+    rps_free(ctx->req);
+    ctx->req = NULL;
     server_do_next(ctx);
 }
 
