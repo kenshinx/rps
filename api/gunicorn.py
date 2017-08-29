1d210e287d81db64e6fb59c1f770b6baa4a8dcef
kenshin <kenshin.acs@gmail.com>
2017-08-29 12:28:11 +0800

    webapi run on gunicorn

---
+++ api/gunicorn.py
@@ -0,0 +1,28 @@
+# Gunicorn configuration file.
+
+bind = '0.0.0.0:9898'
+
+daemon = True
+
+workers = 5
+worker_class = 'sync'
+worker_connections = 1000
+threads = 10
+
+#restart after reach max_requests. bypass the memory continuous climbing up issues.
+max_requests = 5000
+
+timeout = 300
+
+# raw_env = 'RPS_WEB_DEPLOY_MODE=PROD'
+
+proc_name = 'rps'
+
+limit_request_line = 0
+
+accesslog = 'logs/rps.access.log'
+
+access_log_format = '%({X-Real-IP}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(L)ss'
+
+errorlog = 'logs/rps.error.log'
+
