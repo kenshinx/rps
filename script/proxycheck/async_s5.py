90f9bb9f20f8aabf12df079e3e7853c82694aca3
kenshin <kenshin.acs@gmail.com>
2017-09-14 18:32:40 +0800

    Support username:password authentication

--- script/proxycheck/async_s5.py
+++ script/proxycheck/async_s5.py
@@ -10,15 +10,19 @@ from cStringIO import StringIO
 S5_STATE_INIT = 0
 S5_STATE_HANDSHAKE_REQ = 1
 S5_STATE_HANDSHAKE_RESP = 2
-S5_STATE_REQ = 3
-S5_STATE_RESP = 4
-S5_STATE_PAYLOAD_REQ = 5
-S5_STATE_PAYLOAD_RESP = 6
-S5_STATE_CLOSED = 7
+S5_STATE_AUTH_REQ = 3
+S5_STATE_AUTH_RESP = 4
+S5_STATE_REQ = 5
+S5_STATE_RESP = 6
+S5_STATE_PAYLOAD_REQ = 7
+S5_STATE_PAYLOAD_RESP = 8
+S5_STATE_CLOSED = 9
 
 
 SOCKS5_VERSION = 5
+SOCKS5_AUTH_VERSION = 1
 SOCKS5_AUTH_NONE = 0
+SOCKS5_AUTH_PASSWD = 2
 SOCKS5_CMD_CONNECT = 1
 SOCKS5_RSV = 0
 SOCKS5_ATYP_IPV4 = 1
@@ -47,6 +51,10 @@ class Socks5BadVersion(Socks5ProtocolError):
     def __init__(self, version):
         Socks5ProtocolError.__init__(self, "Bad version <%d>" % version)
 
+class Socks5AuthFail(Socks5ProtocolError):
+    def __init__(self):
+        Socks5ProtocolError.__init__(self, "Auth faild.")
+
 class Socks5UnsupportAuth(Socks5ProtocolError):
     def __init__(self, method):
         Socks5ProtocolError.__init__(self, "Unsupport auth method <%d>" % method)
@@ -66,7 +74,7 @@ class AsyncSocks5Client(asyncore.dispatcher):
     HTTP_PAYLOAD = "baidu.com"
     WHOIS_PAYLOAD = "google.com"
 
-    def __init__(self, host, port, remote_host, remote_port, remote_proto, socket_map):
+    def __init__(self, host, port, remote_host, remote_port, remote_proto, socket_map, uname=None, passwd=None):
         asyncore.dispatcher.__init__(self, map = socket_map)
         self.host = host
         self.port = int(port)
@@ -74,6 +82,8 @@ class AsyncSocks5Client(asyncore.dispatcher):
         self.remote_port = remote_port
         self.remote_proto = remote_proto
         self.socket_map = socket_map
+        self.uname = uname
+        self.passwd = passwd
         self.buffer = StringIO()
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.addr = (self.host, self.port)
@@ -107,6 +117,7 @@ class AsyncSocks5Client(asyncore.dispatcher):
         return self.state in [
                               S5_STATE_INIT,
                               S5_STATE_HANDSHAKE_REQ, 
+                              S5_STATE_AUTH_REQ,
                               S5_STATE_REQ, 
                               S5_STATE_PAYLOAD_REQ]
 
@@ -119,6 +130,8 @@ class AsyncSocks5Client(asyncore.dispatcher):
     def handle_write(self):
         if self.state == S5_STATE_HANDSHAKE_REQ:
             self.send_handshake()
+        elif self.state == S5_STATE_AUTH_REQ:
+            self.send_auth(self.uname, self.passwd)
         elif self.state == S5_STATE_REQ:
             self.send_req(self.remote_host, self.remote_port)
         elif self.state == S5_STATE_PAYLOAD_REQ:
@@ -129,6 +142,8 @@ class AsyncSocks5Client(asyncore.dispatcher):
     def handle_read(self):
         if self.state == S5_STATE_HANDSHAKE_RESP:
             self.parse_handshake()
+        elif self.state == S5_STATE_AUTH_RESP:
+            self.parse_auth()
         elif self.state == S5_STATE_RESP:
             self.parse_resp()
         elif self.state == S5_STATE_PAYLOAD_RESP:
@@ -149,7 +164,10 @@ class AsyncSocks5Client(asyncore.dispatcher):
 
 
     def send_handshake(self):
-        buf = "\x05\x01\x00"
+        if self.uname is None:
+            buf = "\x05\x01\x00"
+        else:
+            buf = "\x05\x02\x00\x02"
         self.send(buf)
 
         self.logger.debug("send handshake")
@@ -166,10 +184,39 @@ class AsyncSocks5Client(asyncore.dispatcher):
         version, method = struct.unpack("2B", data)
         if version != SOCKS5_VERSION:
             raise Socks5BadVersion(version)
-        if method != SOCKS5_AUTH_NONE:
+        if method != SOCKS5_AUTH_NONE and method != SOCKS5_AUTH_PASSWD:
             raise Socks5UnsupportAuth(method)
 
-        self.state = S5_STATE_REQ
+        if method == SOCKS5_AUTH_PASSWD:
+            self.state = S5_STATE_AUTH_REQ
+        else:
+            self.state = S5_STATE_REQ
+
+    def send_auth(self, uname, passwd):
+        buf = chr(SOCKS5_AUTH_VERSION) + chr(len(uname)) + uname + chr(len(passwd)) + passwd
+
+        self.send(buf)
+        
+        self.logger.debug("send auth")
+        self.state = S5_STATE_AUTH_RESP
+
+    def parse_auth(self):
+        data = self.recv(2)
+        if len(data) != 2:
+            raise Socks5ProtocolError("invalid auth resp, len: %d" %len(data))
+
+        version, status = struct.unpack("2B", data)
+
+        if version != SOCKS5_AUTH_VERSION and version != SOCKS5_VERSION:
+            raise Socks5AuthFail()
+
+        if status == 0:
+            self.state = S5_STATE_REQ
+        else:
+            raise Socks5AuthFail()
+        
+        pass
+        
 
     def send_req(self, host, port):
         buf = struct.pack("3B", SOCKS5_VERSION, SOCKS5_CMD_CONNECT, SOCKS5_RSV)
@@ -191,7 +238,7 @@ class AsyncSocks5Client(asyncore.dispatcher):
         data = self.recv(4)
         if len(data) != 4:
             raise Socks5ProtocolError("invalid resp, len: %d" %len(data))
-            
+
         version, reply_code, _, addr_type = struct.unpack("4B", data)
 
         if version != SOCKS5_VERSION:
@@ -253,13 +300,15 @@ if __name__ == "__main__":
     WHOIS_HOST = "36.55.244.17"
     WHOIS_PORT = 43
 
+    UNAME = ""
+    PASSWD = ""
     for line in sys.stdin:
         line = line.strip()
         if not line:
             continue
         host, port = line.split(":")
-        clients.append(AsyncSocks5Client(host, port, WHOIS_HOST, WHOIS_PORT, "whois", socket_map))
-        #clients.append(AsyncSocks5Client(host, port, HTTP_HOST, HTTP_PORT, "http", socket_map))
+        clients.append(AsyncSocks5Client(host, port, WHOIS_HOST, WHOIS_PORT, "whois", socket_map, UNAME, PASSWD))
+        #clients.append(AsyncSocks5Client(host, port, HTTP_HOST, HTTP_PORT, "http", socket_map, UNAME, PASSWD))
                 
 
     asyncore.loop(timeout=1, use_poll=True, map=socket_map)
